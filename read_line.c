202e2d6f736fbdf75194ef75c8cdc595030a1049
Ivanzkaa <ivanzka@me.com>
2022-08-04 04:50:28 +0000

    fixing the getline and adding the exit

--- read_line.c
+++ read_line.c
@@ -3,8 +3,12 @@ char *read_line(void)
 {
         char *line = NULL;
         size_t bufsize = 0;
+	ssize_t	get_l;
 
-        printf("$ ");
-        getline(&line, &bufsize, stdin);
+        get_l = getline(&line, &bufsize, stdin);
+
+	if (get_l == -1)
+	exit (0);
+
         return (line);
 }
