7db8dec1fd07ba8aaddf63b41aedce2ac29317d2
AngeiraT <4985@holbertonschool.com>
2022-08-05 13:49:34 +0000

    adding betty comments

--- read_line.c
+++ read_line.c
@@ -7,8 +7,8 @@
 
 char *read_line(void)
 {
-        char *line = NULL;
-        size_t bufsize = 0;
+	char *line = NULL;
+	size_t bufsize = 0;
 	ssize_t	get_l;
 
         get_l = getline(&line, &bufsize, stdin);
